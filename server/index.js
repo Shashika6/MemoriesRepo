9699495ddbb7cf67d4487a1ac453d3377ababc3b
Shashika Boteju <32934169+Shashika6@users.noreply.github.com>
2021-07-03 14:34:41 +0530

    Boiler Plate of backend.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import exress from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -9,3 +9,12 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://mern:mern@123@cluster0.idsjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { app.listen(PORT, () => console.log(`Server connection established on port ${PORT}`)) })
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
